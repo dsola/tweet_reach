a83038f6e69de1b23b68a05839ed93dd7b4fba7a
dsola <d.sola.03@gmail.com>
2018-02-18 20:00:11 +0100

    Add button to try another tweet URL

--- resources/views/tweet_reach_result.blade.php
+++ resources/views/tweet_reach_result.blade.php
@@ -3,5 +3,8 @@
 @section('content')
 <div class="container-fluid center-block" style="width:60%">
     The total reach of this tweet is: <b>{{ $reach }}</b>
+    <div class="form-group">
+        <a href="{{ route('tweet.reach') }}" class="btn btn-primary">Try another one!</a>
+    </div>
 </div>
 @endsection
