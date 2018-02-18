3418f561b1c387ef486e56121246f5951abd19b5
dsola <d.sola.03@gmail.com>
2018-02-18 19:52:11 +0100

    Redirect from / to tweet/reach

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/', 'tweet/reach')->name('tweet.reach');
 
 Route::get('tweet/reach', 'TweetController@displayTweetReach')->name('tweet.reach');
 Route::post('tweet/reach', 'TweetController@processTweet')->name('tweet.reach.process');
